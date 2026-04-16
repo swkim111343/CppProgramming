f64a847b30cfa2edd718a96b1f14c14d3caad5f0
SEONWOO KIM <newswdddddd@gmail.com>
2026-04-16 19:06:53 +0900

    Update test3.cpp

--- ch3-3/test3.cpp
+++ ch3-3/test3.cpp
@@ -4,37 +4,41 @@
 // 작성자 : 2001485 김선우
 // **********************************************
 // 소스코드 작성
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Triangle {
 private:
-    int base;
-    int height;
-
+    int width, height;
 public:
-    Triangle(int b, int h) {
-        base = b;
-        height = h;
-        cout << "밑변 " << base << "높이 " << height << "인 삼각형 생성" << endl;
-    }
+    Triangle();
+    ~Triangle();
+    Triangle(int a, int b);
 
-    double getArea() {
-        return base * height / 2.0;
-    }
-
-    ~Triangle() {
-        cout << "밑변 " << base << "높이 " << height << "인 삼각형 소멸" << endl;
-    }
+    double getArea();
 };
 
-//전역객체 (순서 중요) 실행결과가 다른 이유는 README파일에 실행결과와 함께 설명
-Triangle tri1(4, 8);
-Triangle tri2(2, 2);
+Triangle::Triangle() :Triangle(1, 1) {}
+Triangle::Triangle(int a, int b) :width(a), height(b) {
+    cout << "밑변" << a << "높이" << b << "인 삼각형 생성" << endl;
+}
+Triangle::~Triangle() {
+    cout << "밑변" << width << "높이" << height << "인 삼각형 소멸" << endl;
+}
+
+double Triangle::getArea() {
+    return (1.0 / 2.0) * width * height;
+}
+
+
+//main 함수 안에있는 지역함수를 main 바깥으로 뺌
+Triangle tri1;
+Triangle tri2(2, 4);
 
 int main() {
-    //출력 순서 중요
-    cout << "삼각형의 면적은 " << tri2.getArea() << endl;
-    cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+
+    cout << "tri1의 면적은 " << tri1.getArea() << endl;
+    cout << "tri2의 면적은 " << tri2.getArea() << endl;
+
     return 0;
 }
