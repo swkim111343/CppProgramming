dd82af416a116c837364e4453c96685211a4000b
SEONWOO KIM <newswdddddd@gmail.com>
2026-04-16 19:49:11 +0900

    Update test2.cpp

--- ch3-4/test2.cpp
+++ ch3-4/test2.cpp
@@ -4,32 +4,39 @@
 // 작성자 : 2001485 김선우
 // **********************************************
 // 소스코드 작성
+#include<iostream>
+using namespace std;
+
 class Circle {
 private:
-    int radius;    //멤버변수 private으로 설정    (함수로 접근한다)
-
+	int radius;
 public:
-    Circle();
-    void setRadius(int r);    //setter사용
-    int getRadius();
+	Circle();
+	~Circle();
+	Circle(int r);
+	
+	void setRadius(int r);
+	int getRadius();
 };
 
-Circle::Circle() {
-    radius = 1;
-}
+Circle::Circle():Circle(1){}
+Circle::Circle(int r) :radius(r){}
+Circle::~Circle(){}
 
+// setter
 void Circle::setRadius(int r) {
-    if (r > 0) radius = r;
+	radius = r;
 }
 
+// getter
 int Circle::getRadius() {
-    return radius;
+	return radius;
 }
 
-int main(){   
-    Circle waffle;
-    waffle.setRadius(5);
-    cout << "원의 반지름은 " << waffle.getRadius() << endl;
-
-    return 0;
+int main() {
+	
+	Circle waffle;
+	waffle.setRadius(5);
+	cout << "waffle의 반지름은 " << waffle.getRadius() << endl;
+	return 0;
 }
