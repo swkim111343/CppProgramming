6b04052002024299e4f28fbada86e8fda8f3384d
SEONWOO KIM <newswdddddd@gmail.com>
2026-04-16 19:16:45 +0900

    Refactor Sphere constructor and main function usage

--- ch3-3/test4.cpp
+++ ch3-3/test4.cpp
@@ -18,16 +18,17 @@ public:
 	double getVolume();
 };
 
-Sphere::Sphere():Sphere(1){}    //생성자
-Sphere::Sphere(int r) :radius(r) { cout << "반지름" << radius << "인 구 생성" << endl; }//멤버 초기화리스트
-Sphere::~Sphere() { cout << "반지름" << radius << "인 구 소멸" << endl; }    //소멸자
+Sphere::Sphere():Sphere(1){}
+Sphere::Sphere(int r) :radius(r) { cout << "반지름" << radius << "인 구 생성" << endl; }
+Sphere::~Sphere() { cout << "반지름" << radius << "인 구 소멸" << endl; }
 
 double Sphere::getVolume() { return (4.0 / 3.0) * (radius*radius * radius) * 3.14; }
 // 클래스 정의
+
+Sphere sph1; // 반지름 =1로 초기화
+Sphere sph2(3); // 반지름 =3로 초기화
 int main() {
-	Sphere sph1; // 반지름 =1로 초기화
 	cout << "구의 부피는" << sph1.getVolume() << endl;
-		Sphere sph2(3); // 반지름 =3로 초기화
 	cout << "구의 부피는" << sph2.getVolume () << endl;
 		return 0;
 }
