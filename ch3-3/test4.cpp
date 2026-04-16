7ffc58f2166209965ca081a925298fa259269b7d
SEONWOO KIM <newswdddddd@gmail.com>
2026-04-16 19:16:00 +0900

    Update test4.cpp

--- ch3-3/test4.cpp
+++ ch3-3/test4.cpp
@@ -4,43 +4,30 @@
 // 작성자 : 2001485 김선우
 // **********************************************
 // 소스코드 작성
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Sphere {
 private:
-    int radius;
-
+	int radius;
 public:
-    // 기본 생성자
-    Sphere() {
-        radius = 1;
-        cout << "반지름 " << radius << "인 구 생성" << endl;
-    }
+	Sphere();
+	~Sphere();
+	Sphere(int r);
 
-    // 매개변수 생성자
-    Sphere(int r) {
-        radius = r;
-        cout << "반지름 " << radius << "인 구 생성" << endl;
-    }
-
-    // 부피 계산 함수
-    double getVolume() {
-        return (4.0 / 3.0) * 3.14 * radius * radius * radius;
-    }
-
-    // 소멸자
-    ~Sphere() {
-        cout << "반지름 " << radius << "인 구 소멸" << endl;
-    }
+	double getVolume();
 };
 
+Sphere::Sphere():Sphere(1){}    //생성자
+Sphere::Sphere(int r) :radius(r) { cout << "반지름" << radius << "인 구 생성" << endl; }//멤버 초기화리스트
+Sphere::~Sphere() { cout << "반지름" << radius << "인 구 소멸" << endl; }    //소멸자
+
+double Sphere::getVolume() { return (4.0 / 3.0) * (radius*radius * radius) * 3.14; }
 // 클래스 정의
 int main() {
-    Sphere sph1; // 반지름 =1로 초기화
-    cout << "구의 부피는" << sph1.getVolume() << endl;
-
-    Sphere sph2(3); // 반지름 =3로 초기화
-    cout << "구의 부피는" << sph2.getVolume () << endl;
-        return 0;
+	Sphere sph1; // 반지름 =1로 초기화
+	cout << "구의 부피는" << sph1.getVolume() << endl;
+		Sphere sph2(3); // 반지름 =3로 초기화
+	cout << "구의 부피는" << sph2.getVolume () << endl;
+		return 0;
 }
